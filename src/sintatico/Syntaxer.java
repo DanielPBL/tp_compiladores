6882d802b5e39692e49933d71bb2c7da02980091
Daniel Lopes <danielllopes11@gmail.com>
2017-05-02 17:42:50 -0300

    Início Sintático

---
+++ src/sintatico/Syntaxer.java
@@ -0,0 +1,101 @@
+package sintatico;
+
+public class Syntaxer {
+
+    public void program() {
+    }
+
+    public void programSuffix() {
+    }
+
+    public void declList() {
+    }
+
+    public void declListTail() {
+    }
+
+    public void decl() {
+    }
+
+    public void identList() {
+    }
+
+    public void identListTail() {
+    }
+
+    public void type() {
+    }
+
+    public void stmtList() {
+    }
+
+    public void stmtListTail() {
+    }
+
+    public void stmt() {
+    }
+
+    public void assignStmt() {
+    }
+
+    public void ifStmt() {
+    }
+
+    public void ifSuffix() {
+    }
+
+    public void condition() {
+    }
+
+    public void doStmt() {
+    }
+
+    public void doSuffix() {
+    }
+
+    public void readStmt() {
+    }
+
+    public void writeStmt() {
+    }
+
+    public void writeble() {
+    }
+
+    public void expression() {
+    }
+
+    public void expressionSuffix() {
+    }
+
+    public void simpleExpr() {
+    }
+
+    public void simpleExprPrime() {
+    }
+
+    public void term() {
+    }
+
+    public void termPrime() {
+    }
+
+    public void factorA() {
+    }
+
+    public void factor() {
+    }
+
+    public void relOp() {
+    }
+
+    public void appOp() {
+    }
+
+    public void mulOp() {
+    }
+
+    public void constant() {
+    }
+
+}
