6f74d28023e4bae9eee805310ae3e176f4b59bfc
Daniel Lopes <danielllopes11@gmail.com>
2017-05-03 17:42:09 -0300

    Implementação da gramática

--- src/sintatico/Syntaxer.java
+++ src/sintatico/Syntaxer.java
@@ -1,14 +1,97 @@
 package sintatico;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import lexico.Lexer;
+import lexico.LexicalException;
+import lexico.Tag;
+import lexico.Token;
+
 public class Syntaxer {
 
+    private final Lexer lexer;
+    private Token token;
+
+    public Syntaxer(Lexer lexer) {
+        this.lexer = lexer;
+        this.advance();
+    }
+
+    private void advance() {
+        try {
+            this.token = this.lexer.scan();
+        } catch (IOException | LexicalException ex) {
+            Logger.getLogger(Syntaxer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void eat(int tag) {
+        if (this.token.tag == tag) {
+            this.advance();
+        } else {
+            this.error();
+        }
+    }
+    
+    private void error() {
+        System.out.println("Erro Sintático");
+        System.exit(1);
+    }
+
     public void program() {
+        this.eat(Tag.INIT);
+        this.programSuffix();
+        this.eat(Tag.EOF);
     }
 
     public void programSuffix() {
+        switch (this.token.tag) {
+            case Tag.ID:
+                this.eat(Tag.ID);
+                this.declAssign();
+                break;
+            case Tag.IF:
+            case Tag.DO:
+            case Tag.READ:
+            case Tag.WRITE:
+                this.otherStmt();
+                break;
+            default:
+                this.error();
+        }
     }
 
-    public void declList() {
+    public void declAssign() {
+        switch (this.token.tag) {
+            case Tag.ATRIB:
+                this.eat(Tag.ATRIB);
+                this.simpleExpr();
+                this.eat(';');
+                this.stmtListTail();
+                this.eat(Tag.STOP);
+                break;
+            case ',':
+            case Tag.IS:
+                this.identListTail();
+                this.eat(Tag.IS);
+                this.type();
+                this.eat(';');
+                this.declListTail();
+                this.stmtList();
+                this.eat(Tag.STOP);
+                break;
+            default:
+                this.error();
+        }
+    }
+
+    public void otherStmt() {
+
+    }
+
+    public void stmtPrime() {
+
     }
 
     public void declListTail() {
